64eda8d63f64f82fe10efcaac1e99758fce1f6b1
Seungheon Oh <dan.oh0721@gmail.com>
2020-06-28 23:04:03 -0500

    fixed bytebuffer.h

--- bytebuffer.h
+++ bytebuffer.h
@@ -10,19 +10,35 @@ typedef struct {
 bytebuffer* bytebuffer_init(size_t s) {
   bytebuffer* b = malloc(sizeof(bytebuffer));
   b->alloc_size = s;
-  b->buf = malloc(b->alloc_size);
+  b->buf = calloc(b->alloc_size, sizeof(char));
   b->p = b->buf;
   return b;
 }
 
-void bytebuffer_append(bytebuffer* b, char* c, size_t s) {
-  if((b->p - b->buf)+s >= b->alloc_size) {
-    b->buf = realloc(b->buf, b->alloc_size * 2);
-    b->alloc_size *= 2;
-  }
-  memcpy(b->p, c, s);
+size_t bytebuffer_size(bytebuffer* b) {
+  return b->p - b->buf;
 }
 
-void bytebuffer_clean(bytebuffer* b) {
+void _bytebuffer_alloc(bytebuffer* b, size_t s) {
+  if(bytebuffer_size(b) + s < b->alloc_size) return;
+  if(b->alloc_size * 2 < 0) return;
+
+  char* newptr = realloc(b->buf, b->alloc_size * 2);
+  b->alloc_size *= 2;
+  b->p = newptr + bytebuffer_size(b);
+  b->buf = newptr;
+}
+
+void bytebuffer_append(bytebuffer* b, char* c, size_t s) {
+  _bytebuffer_alloc(b, s);
+  memcpy(b->p, c, s);
+  b->p += s;
+}
+
+void bytebuffer_clear(bytebuffer* b) {
   b->buf = b->p;
+}
+
+void bytebuffer_free(bytebuffer* b) {
+  free(b->buf);
 }
