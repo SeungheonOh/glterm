dfeffa5212dce0b22e0cc9de11b5faf73f126fac
Seungheon Oh <dan.oh0721@gmail.com>
2020-07-05 02:11:27 -0500

    organized control code handling

--- parse.h
+++ parse.h
@@ -8,15 +8,15 @@
  * https://www.inwap.com/pdp10/ansicode.txt
  *
  * Character types:
- *  C0 Control	    00-1F hex  (G0 is 041-176 octal, 21-7E hex)
- *  SPACE		        20+A0 hex  Always and everywhere a blank space
- *  Intermediate	  20-2F hex   !"#$%&'()*+,-./
- *  Parameters	    30-3F hex  0123456789:;<=>?
- *  Uppercase	      40-5F hex  @ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_
- *  Lowercase	      60-7E hex  `abcdefghijlkmnopqrstuvwxyz{|}~
- *  Alphabetic	    40-7E hex  (all of upper and lower case)
- *  Delete		      7F    hex  Always and everywhere ignored
- *  C1 Control	    80-9F hex  32 additional control characters <- not sure about this
+ *  C0 Control      00-1F hex  (G0 is 041-176 octal, 21-7E hex)
+ *  SPACE           20+A0 hex  Always and everywhere a blank space
+ *  Intermediate    20-2F hex   !"#$%&'()*+,-./
+ *  Parameters      30-3F hex  0123456789:;<=>?
+ *  Uppercase       40-5F hex  @ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_
+ *  Lowercase       60-7E hex  `abcdefghijlkmnopqrstuvwxyz{|}~
+ *  Alphabetic      40-7E hex  (all of upper and lower case)
+ *  Delete          7F    hex  Always and everywhere ignored
+ *  C1 Control      80-9F hex  32 additional control characters <- not sure about this
  *
  * Sequences:
  * control character
@@ -43,7 +43,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-enum CHARACTER_TYPE{
+#include "control_characters.h"
+#include "sequences.h"
+
+enum CHARACTER_TYPE {
   C0_CONTROL,   //00-1F 0 - 31
   SPACE,        //32
   INTERMEDIATE, //20-2F 32 - 47
@@ -65,7 +68,11 @@ enum CHARACTER_TYPE char_categorize(char c) {
   else return OTHER;
 }
 
-bool esc_validate(enum ESC_STATE state, char c) {
+/*
+ * Check if character is vaild for state given
+ * read comments on the top
+ */
+bool esc_is_valid(enum ESC_STATE state, char c) {
   enum CHARACTER_TYPE chtype = char_categorize(c);
   if(state == STATE_NEUTRAL) return true;
   else if(state == STATE_ESC) {
@@ -85,9 +92,14 @@ bool esc_validate(enum ESC_STATE state, char c) {
   return false;
 }
 
+/*
+ * Check sequence is finished or not
+ * read comments on the top
+ */
 bool esc_is_complete(struct esc_sequence esc) {
+  int buf_size = bytebuffer_size(esc.buf);
   enum ESC_STATE state = esc.state;
-  enum CHARACTER_TYPE chtype = char_categorize(esc.buf->buf[bytebuffer_size(esc.buf) - 1]);
+  enum CHARACTER_TYPE chtype = char_categorize(esc.buf->b[bytebuffer_size(esc.buf) - 1]);
   if(state == STATE_NEUTRAL) return true;
   else if(state == STATE_ESC) {
     if(chtype == PARAMETER
@@ -98,15 +110,11 @@ bool esc_is_complete(struct esc_sequence esc) {
     if(chtype == UPPERCASE
         || chtype == LOWERCASE) return true;
     else return false;
-  } else if(state == STATE_DCS) {
-    if(esc.buf->buf[bytebuffer_size(esc.buf) - 1] == '\007'
-        || (esc.buf->buf[bytebuffer_size(esc.buf) - 1] == '\\'
-        && esc.buf->buf[bytebuffer_size(esc.buf) - 2] == '\033')) return true;
-    else return false;
-  } else if(state == STATE_OSC) {
-    if(esc.buf->buf[bytebuffer_size(esc.buf) - 1] == '\007'
-        || (esc.buf->buf[bytebuffer_size(esc.buf) - 1] == '\\'
-        && esc.buf->buf[bytebuffer_size(esc.buf) - 2] == '\033')) return true;
+  } else if(state == STATE_DCS || state == STATE_OSC) {
+    if(buf_size < 2) return false;
+    if(esc.buf->b[bytebuffer_size(esc.buf) - 1] == '\007'
+        || (esc.buf->b[bytebuffer_size(esc.buf) - 1] == '\\'
+        && esc.buf->b[bytebuffer_size(esc.buf) - 2] == '\033')) return true;
     else return false;
   } else return true;
 }
@@ -117,13 +125,19 @@ void esc_reset(struct terminal* t) {
   t->esc.state = STATE_NEUTRAL;
 }
 
+/*
+ * 1. When the buffer is complete ( decided by esc_is_complete() ), return true
+ * to run sequence handler.
+ *
+ * 2. Append given character to terminal->esc.buf, ONLY if when c is validated by
+ * esc_is_valid(). When it's not valid, the buffer will be reset.
+ */
 bool esc_append(struct terminal* t, char c) {
   bytebuffer_append(t->esc.buf, &c, 1);
   if (esc_is_complete(t->esc)) return true;
-  if(!esc_validate(t->esc.state, c)) {
-    log_debug("invalid sequence");
+  if(!esc_is_valid(t->esc.state, c)) {
+    log_debug("invalid sequence: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->b);
     esc_reset(t);
-    return false;
   }
   return false;
 }
@@ -133,16 +147,15 @@ bool esc_append(struct terminal* t, char c) {
  * like SOH, XON, ESC, CR, etc...
  *
  * change state to STATE_ESC when ESC is given
+ *
+ * Note: control characters are not being pushed into terminal->esc.buf
  */
 void control_character(struct terminal* t, char c) {
   t->esc.state = STATE_NEUTRAL;
-  if(c == '\033')t->esc.state = STATE_ESC;
-  else if(c == '\n')terminal_cursor_next_line(t);
-  else if(c == 8) {
-    t->cursor.x--;
-    terminal_erase_cell(t, t->cursor.x, t->cursor.y);
-  }
-  log_debug("control character : %d", c);
+  if(handle_control_character(c))
+    handle_control_character(c)(t);
+  else
+    log_debug("Control code not found: %d", c);
 }
 
 /*
@@ -165,14 +178,14 @@ void escape_sequence(struct terminal* t, char c) {
 }
 
 void control_sequence(struct terminal* t, char c) {
-  log_debug("CSI complete: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->buf);
+  log_debug("CSI complete: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->b);
 
   bytebuffer_append(t->esc.buf, "\0", 1);
-  if(strcmp(t->esc.buf->buf, "[H") == 0) {
+  if(strcmp(t->esc.buf->b, "[H") == 0) {
     terminal_set_cursor(t, 0, 0);
-  } else if(strcmp(t->esc.buf->buf, "[2J") == 0) {
+  } else if(strcmp(t->esc.buf->b, "[2J") == 0) {
     terminal_clear(t);
-  } else if(strcmp(t->esc.buf->buf, "[C") == 0) {
+  } else if(strcmp(t->esc.buf->b, "[C") == 0) {
     t->cursor.x--;
     terminal_erase_cell(t, t->cursor.x, t->cursor.y);
   }
@@ -180,12 +193,12 @@ void control_sequence(struct terminal* t, char c) {
 }
 
 void device_control_sequence(struct terminal* t, char c) {
-  log_debug("DCS complete: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->buf);
+  log_debug("DCS complete: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->b);
   esc_reset(t);
 }
 
 void operating_system_command(struct terminal* t, char c) {
-  log_debug("OSC complete: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->buf);
+  log_debug("OSC complete: %.*s", bytebuffer_size(t->esc.buf), t->esc.buf->b);
   esc_reset(t);
 }
 
